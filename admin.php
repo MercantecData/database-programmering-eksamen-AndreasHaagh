3badfb677af32451e9df0214ae837086f36ef8cc
Andreas Flyvholm Haagh <afh@projectum.dk>
2018-02-14 11:44:22 +0100

    password hashing on admin and login pages

--- admin.php
+++ admin.php
@@ -2,15 +2,32 @@
 
 if(isset($_POST["submit"])) {
 	$conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$sql = "SELECT id FROM adminusers WHERE username = '$username' AND password = '$password'";
+
+	$username = $conn->real_escape_string($_POST["username"]);
+	$password = $conn->real_escape_string($_POST["password"]);
+
+	$sql = "SELECT id, password FROM adminusers WHERE username = '$username'";
 	$result = $conn->query($sql);
+
 	if($result->num_rows > 0) {
-		header("Location: userlist.php");
+		$row = $result->fetch_assoc();
+		$id = $row['id'];
+
+		if ($password == $row['password']) {
+			$hash = password_hash($row['password'], PASSWORD_DEFAULT);
+        	$sqlUpdate = "UPDATE adminusers SET password = '$hash' WHERE id = $id";
+			$result = $conn->query($sqlUpdate);
+			header("Location: userlist.php");
+		}
+
+		if (password_verify($password, $row['password'])) {
+			header("Location: userlist.php");
+		} else {
+			echo "<p style='color:red'>Wrong password</p>";
+		}
 		exit;
 	} else {
-		echo "<p style='color:red'>Wrong Username/Password</p>";
+		echo "<p style='color:red'>Wrong Username</p>";
 	}
 }
 ?>
--- login.php
+++ login.php
@@ -1,17 +1,38 @@
 <?php
-session_start();
-$usrname = $_POST["username"];
-$password = $_POST["password"];
 
 $conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
 
-$sql = "SELECT id, name FROM users WHERE username = '$usrname' AND password = '$password'";
-echo $sql . "<br>";
+$usrname = $conn->real_escape_string($_POST["username"]);
+$password = $conn->real_escape_string($_POST["password"]);
+
+$sql = "SELECT id, name, password FROM users WHERE username = '$usrname'";
 $result = $conn->query($sql);
-$row = $result->fetch_assoc();
-var_dump($row);
-$id = $row["id"];
-$name = $row["name"];
-$_SESSION['userID'] = $id;
-$_SESSION["userName"] = $name;
-header("Location: index.php");//redirects back
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $id = $row["id"];
+    $name = $row["name"];
+
+    if ($password == $row['password']) {
+        $hash = password_hash($row['password'], PASSWORD_DEFAULT);
+        $sqlUpdate = "UPDATE users SET password = '$hash' WHERE id = $id";
+        $result = $conn->query($sqlUpdate);
+
+        start_session($id, $name);
+    }
+
+    if (password_verify($password, $row['password'])) {
+        start_session($id, $name);
+    } else {
+        echo "<p style='color:red'>Wrong password</p>";
+    }
+} else {
+    echo "<p style='color:red'>Wrong username</p>";
+}
+
+function start_session($id, $name) {
+    session_start();
+    $_SESSION['userID'] = $id;
+    $_SESSION["userName"] = $name;
+    header("Location: index.php");//redirects back
+}
